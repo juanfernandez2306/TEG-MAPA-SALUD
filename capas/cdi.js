70e9f4c0b91c6a7f8b16c4607aa79ace95ffdc4d
juanfernandez2306 <50415310+juanfernandez2306@users.noreply.github.com>
2019-05-08 13:20:52 -0400

    Agregando archivos
    agregando archivos

---
+++ capas/cdi.js
@@ -0,0 +1,11 @@
+var cdi = {
+"type": "FeatureCollection",
+"name": "cdi",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "cod_temp": 110701, "asic": "LUIS DE VICENTE", "municipio": "MARA", "parroquia": "LUIS DE VICENTE", "comunidad": null, "direccion": "CARRASQUERO, AV. PRINCIPAL, SECTOR LAGRIMAS VERDES.", "nombre": "CDI LUIS DE VICENTE", "tipo": "CENTRO MEDICO DE DIAGNOSTICO INTEGRAL COMUNITARIO", "observ": null, "foto": 1, "url_foto": "https:\/\/i.ibb.co\/SdMY1X9\/110701.jpg" }, "geometry": { "type": "Point", "coordinates": [ -72.00775, 11.03228 ] } },
+{ "type": "Feature", "properties": { "cod_temp": 115401, "asic": "LA SIERRITA", "municipio": "MARA", "parroquia": "LA SIERRITA", "comunidad": null, "direccion": "CARRETERA PRINCIPAL VIA A FUERTE MARA SECTOR LA SIERRITA ", "nombre": "CDI SAN JOSE DE LA SIERRITA", "tipo": "CENTRO MEDICO DE DIAGNOSTICO INTEGRAL COMUNITARIO", "observ": null, "foto": 1, "url_foto": "https:\/\/i.ibb.co\/6N923RK\/115401.jpg" }, "geometry": { "type": "Point", "coordinates": [ -71.86315, 10.82531 ] } },
+{ "type": "Feature", "properties": { "cod_temp": 115501, "asic": "SAN RAFAEL DEL MOJAN", "municipio": "MARA", "parroquia": "SAN RAFAEL", "comunidad": null, "direccion": "SAN RAFAEL DEL MOJAN,AV. PRINC. EL MOJAN, FRENTE A PLAZA EL ANGEL ", "nombre": "CDI SAN RAFAEL ", "tipo": "CENTRO MEDICO DE DIAGNOSTICO INTEGRAL COMUNITARIO", "observ": null, "foto": 1, "url_foto": "https:\/\/i.ibb.co\/5vQqq4J\/115501.jpg" }, "geometry": { "type": "Point", "coordinates": [ -71.73583, 10.95755 ] } },
+{ "type": "Feature", "properties": { "cod_temp": 115701, "asic": "SANTA CRUZ DE MARA", "municipio": "MARA", "parroquia": "RICAURTE", "comunidad": "SANTA CRUZ DE MARA", "direccion": "AV PRINCIPAL SANTA CRUZ DE MARA FRENTE A PLAZA BOLIVAR", "nombre": "CDI SANTA CRUZ DE MARA", "tipo": "CENTRO MEDICO DE DIAGNOSTICO INTEGRAL COMUNITARIO", "observ": null, "foto": 1, "url_foto": "https:\/\/i.ibb.co\/LY3QL4p\/115701.jpg" }, "geometry": { "type": "Point", "coordinates": [ -71.68059, 10.79165 ] } }
+]
+}
