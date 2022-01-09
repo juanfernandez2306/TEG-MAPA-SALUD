785470109614d238bcf3fcc33d9a308c3c344958
juanfernandez2306 <juanfernandez2306@gmail.com>
2022-01-09 14:06:14 -0400

    creando efecto traslate sidebar

--- javascript-css/index.js
+++ javascript-css/index.js
@@ -1,9 +1,16 @@
 function closeSidebar(e){
 	e.preventDefault();
+	var btn = e.target;
 	const sidebar = document.querySelector('div.sidebar');
 	
-	sidebar.classList.remove('showSidebar');
-	sidebar.classList.add('hiddenSidebar');
+	btn.disabled = true;
+	sidebar.classList.add('translate-background');
+	setTimeout(function(){
+		sidebar.classList.remove('translate-background');
+		sidebar.classList.remove('showSidebar');
+		sidebar.classList.add('hiddenSidebar');
+		btn.removeAttribute('disabled');
+	}, 2000);
 }
 
 function init(){
